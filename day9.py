f23377d5ebf814ed3d7b71fb5d29592ead598aac
ffaure32 <frfaure32@gmail.com>
2021-12-09 08:58:00 +0100

    que c'est moche

---
+++ day9.py
@@ -0,0 +1,124 @@
+import numpy
+
+from utils.file_utils import get_lines
+
+
+class PointWithNeighbours():
+    value: int
+    neighbours: set
+
+    def __init__(self, lines, num_row, num_col):
+        self.neighbours = set()
+        self.value = int(lines[num_row][num_col])
+        if num_row > 0:
+            self.neighbours.add(int(lines[num_row - 1][num_col]))
+        if num_row < len(lines) - 1:
+            self.neighbours.add(int(lines[num_row + 1][num_col]))
+        if num_col > 0:
+            self.neighbours.add(int(lines[num_row][num_col - 1]))
+        if num_col < len(lines[0]) - 1:
+            self.neighbours.add(int(lines[num_row][num_col + 1]))
+
+    def risk_level(self):
+        if self.value < min(self.neighbours):
+            return 1 + self.value
+        else:
+            return 0
+
+
+def test_sample():
+    lines = ['2199943210',
+             '3987894921',
+             '9856789892',
+             '8767896789',
+             '9899965678']
+    assert calculate_risk_level(lines) == 15
+
+
+def test_input():
+    lines = get_lines('day9.txt')
+    assert calculate_risk_level(lines) == 15
+
+
+def calculate_risk_level(lines):
+    points = set()
+    for num_line, row in enumerate(lines):
+        for num_col, row in enumerate(row):
+            points.add(PointWithNeighbours(lines, num_line, num_col))
+    return sum([point.risk_level() for point in points])
+
+
+def test_sample_part2():
+    lines = ['2199943210',
+             '3987894921',
+             '9856789892',
+             '8767896789',
+             '9899965678']
+    builder = BasinBuilder(lines)
+    assert builder.find_biggest_basins() == 1134
+
+
+def test_input_part2():
+    lines = get_lines('day9.txt')
+    builder = BasinBuilder(lines)
+    assert builder.find_biggest_basins() == 1134
+
+
+class BasinBuilder:
+    coords_in_basin: set
+    basins: set
+
+    def __init__(self, lines):
+        self.lines = lines
+        self.coords_in_basin = set()
+        self.basins = set()
+        for num_row, row in enumerate(lines):
+            for num_col, row in enumerate(row):
+                new_coord = (num_col, num_row)
+                value = int(lines[num_row][num_col])
+                if new_coord not in self.coords_in_basin and value < 9:
+                    basin = self.build_basin(lines, num_col, num_row)
+                    self.coords_in_basin.update(basin.points_in_basin)
+                    self.basins.add(basin)
+
+    def build_basin(self, lines, num_col, num_row):
+        return Basin(lines, num_col, num_row)
+
+
+    def find_biggest_basins(self):
+        sorted_basins = sorted(self.basins)
+        last_elements = sorted_basins[-3:]
+        return numpy.prod([basin.size() for basin in last_elements])
+
+
+class Basin:
+    points_in_basin: set
+
+    def __init__(self, lines, num_col, num_row):
+        self.lines = lines
+        self.points_in_basin = set()
+        self.points_in_basin.add((num_col, num_row))
+        self.add_to_basin(num_col, num_row)
+
+    def add_to_basin(self, num_col, num_row):
+        if num_row > 0:
+            self.add_to_basin_with_coords(num_col, num_row - 1)
+        if num_row < len(self.lines) - 1:
+            self.add_to_basin_with_coords(num_col, num_row + 1)
+        if num_col > 0:
+            self.add_to_basin_with_coords(num_col - 1, num_row)
+        if num_col < len(self.lines[0]) - 1:
+            self.add_to_basin_with_coords(num_col + 1, num_row)
+
+    def add_to_basin_with_coords(self, num_col, num_row):
+        point_to_add = (num_col, num_row)
+        if point_to_add not in self.points_in_basin and int(self.lines[num_row][num_col]) < 9:
+            self.points_in_basin.add(point_to_add)
+            self.add_to_basin(num_col, num_row)
+
+    def size(self):
+        return len(self.points_in_basin)
+
+
+    def __lt__(self, other):
+        return len(self.points_in_basin) < len(other.points_in_basin)
