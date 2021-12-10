2af5fac2bba544ebd283332fa621db02583cf3d2
ffaure32 <frfaure32@gmail.com>
2021-12-10 10:26:06 +0100

    refacto mais toujours aussi moche

--- day10.py
+++ day10.py
@@ -14,6 +14,7 @@ def remove_closest_chunks(line):
 
 
 closing_chars = {')': 3, ']': 57, '}': 1197, '>': 25137}
+opening_chars = {'(': 1, '[': 2, '{': 3, '<': 4}
 closing_chars_list = list(closing_chars.keys())
 valid_line = [-1, -1, -1, -1]
 
@@ -36,21 +37,20 @@ def index_of_min_value(indexes):
 
 
 def clean_lines_part2(lines):
-    opening_chars = ['(', '[', '{', '<']
     cleaned_lines = [remove_closest_chunks(line) for line in lines]
     results = set()
     for cleaned_line in cleaned_lines:
         indexes = [cleaned_line.find(closing_char) for closing_char in closing_chars_list]
         if indexes == valid_line:
-            results.add(compute_completion_string_score(cleaned_line, opening_chars))
+            results.add(compute_completion_string_score(cleaned_line))
     index = int(len(results) / 2)
     return sorted(results)[index]
 
 
-def compute_completion_string_score(cleaned_line, opening_chars):
+def compute_completion_string_score(cleaned_line):
     total = 0
     for char in reversed(cleaned_line):
-        total = total * 5 + opening_chars.index(char) + 1
+        total = total * 5 + opening_chars[char]
     return total
 
 
