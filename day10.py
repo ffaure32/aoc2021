66cd1377d402cb1e3ae7886d2555fa9845ac7beb
ffaure32 <frfaure32@gmail.com>
2021-12-10 09:48:17 +0100

    day 10 sans stack c'est moche

---
+++ day10.py
@@ -0,0 +1,75 @@
+from utils.file_utils import get_lines
+
+
+def remove_closest_chunks(line):
+    removes = True
+    while removes:
+        original_len = len(line)
+        line = line.replace('{}', '')
+        line = line.replace('()', '')
+        line = line.replace('[]', '')
+        line = line.replace('<>', '')
+        removes = len(line) != original_len
+    return line
+
+
+closing_chars = {')': 3, ']': 57, '}': 1197, '>': 25137}
+closing_chars_list = list(closing_chars.keys())
+valid_line = [-1, -1, -1, -1]
+
+
+def clean_lines(lines):
+    cleaned_lines = [remove_closest_chunks(line) for line in lines]
+    result = 0
+    for cleaned_line in cleaned_lines:
+        indexes = [cleaned_line.find(closing_char) for closing_char in closing_chars_list]
+        if indexes != valid_line:
+            index = index_of_min_value(indexes)
+            result += closing_chars[closing_chars_list[index]]
+    return result
+
+
+def index_of_min_value(indexes):
+    max_i = max(indexes)
+    indexes = [max_i + 1 if index == -1 and max_i != -1 else index for index in indexes]
+    return indexes.index(min(indexes))
+
+
+def clean_lines_part2(lines):
+    opening_chars = ['(', '[', '{', '<']
+    cleaned_lines = [remove_closest_chunks(line) for line in lines]
+    results = set()
+    for cleaned_line in cleaned_lines:
+        indexes = [cleaned_line.find(closing_char) for closing_char in closing_chars_list]
+        if indexes == valid_line:
+            results.add(compute_completion_string_score(cleaned_line, opening_chars))
+    index = int(len(results) / 2)
+    return sorted(results)[index]
+
+
+def compute_completion_string_score(cleaned_line, opening_chars):
+    total = 0
+    for char in reversed(cleaned_line):
+        total = total * 5 + opening_chars.index(char) + 1
+    return total
+
+
+def test_sample():
+    lines = ['[({(<(())[]>[[{[]{<()<>>',
+             '[(()[<>])]({[<{<<[]>>(',
+             '{([(<{}[<>[]}>{[]{[(<()>',
+             '(((({<>}<{<{<>}{[]{[]{}',
+             '[[<[([]))<([[{}[[()]]]',
+             '[{[{({}]{}}([{[{{{}}([]',
+             '{<[[]]>}<{[{[{[]{()[[[]',
+             '[<(<(<(<{}))><([]([]()',
+             '<{([([[(<>()){}]>(<<{{',
+             '<{([{{}}[<[[[<>{}]]]>[]]']
+    assert clean_lines_part2(lines) == 288957
+    assert (clean_lines(lines)) == 26397
+
+
+def test_input():
+    lines = get_lines('day10.txt')
+    assert (clean_lines(lines)) == 315693
+    assert clean_lines_part2(lines) == 1870887234
