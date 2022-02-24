680c07225af3ff4b28046f3d25c8f06349122de4
ffaure32 <frfaure32@gmail.com>
2022-02-24 19:28:16 +0100

    output

--- hashcode.py
+++ hashcode.py
@@ -3,16 +3,19 @@ import numpy as np
 from utils.file_utils import get_lines
 
 
-def test_read_input():
-    input = get_lines("a_an_example.in.txt")
 
+def test_read_input():
+    hash_code("b_better_start_small.in.txt")
+    #hash_code("a_an_example.in.txt")
+
+
+def hash_code(input_file):
+    input = get_lines(input_file)
     line1 = input[0].split(' ')
     nb_gens = int(line1[0])
     nb_projets = int(line1[1])
-
     persons = list()
     projects = list()
-
     line = 1
     for _ in range(nb_gens):
         gen = input[line].split(' ')
@@ -27,7 +30,6 @@ def test_read_input():
             niveau = int(tech[1])
             person.add_techno(techno, niveau)
             line += 1
-
     for _ in range(nb_projets):
         proj = input[line].split(' ')
         project = Project(proj[0], int(proj[1]), int(proj[2]), int(proj[3]), int(proj[4]))
@@ -39,8 +41,37 @@ def test_read_input():
             niveau = int(tech[1])
             project.add_techno(techno, niveau)
             line += 1
+    print()
 
-    print(projects)
+    outputs = list()
+    for project in projects:
+        output = Output(project.name)
+        for techno in project.technos.keys():
+            for person in persons:
+                if techno in person.technos.keys() and person.technos[techno] >= project.technos[techno]:
+                    output.add_person(person.name)
+        if len(output.persons) > 0:
+            outputs.append(output)
+
+    print(len(outputs))
+    for output in outputs:
+        output.print()
+
+class Output:
+
+    def __init__(self, project_name) -> None:
+        super().__init__()
+        self.project_name = project_name
+        self.persons = list()
+
+    def add_person(self, name):
+        self.persons.append(name)
+
+    def print(self):
+        print(self.project_name)
+        print(' '.join(self.persons))
+
+
 
 class Person:
     def __init__(self, name, nb_technos) -> None:
