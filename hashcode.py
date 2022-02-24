40e8cc723c3025f4e04b70a1572316c16bdd56c2
ffaure32 <frfaure32@gmail.com>
2022-02-24 19:06:01 +0100

    struct

--- hashcode.py
+++ hashcode.py
@@ -10,6 +10,7 @@ def test_read_input():
     nb_gens = int(line1[0])
     nb_projets = int(line1[1])
 
+    persons = list()
 
     line = 1
     for i in range(nb_gens):
@@ -17,11 +18,35 @@ def test_read_input():
         name = gen[0]
         nb_technos = int(gen[1])
         line += 1
+        person = Person(name)
+        persons.append(person)
         for j in range(nb_technos):
             tech = input[line].split(' ')
             techno = tech[0]
-            niveau = tech[1]
+            niveau = int(tech[1])
+            person.add_techno(techno, niveau)
             line +=1
 
     for i in range(nb_projets):
         pass
+
+
+class Person:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.technos = dict()
+
+    def add_techno(self, techno, level):
+        self.technos[techno] = level
+
+class Project:
+    def __init__(self, name, length, score, best_before) -> None:
+        super().__init__()
+        self.name = name
+        self.technos = dict()
+        self.length = length
+        self.score = score
+        self.best_before = best_before
+
+    def add_techno(self, techno, level):
+        self.technos[techno] = level
